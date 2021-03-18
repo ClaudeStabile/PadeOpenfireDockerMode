4dc938d95abdec98598e7348e59aacbf186cbd20
root <root@vidchat.oger.ch>
2021-03-18 11:45:55 +0100

    Updated pre-conf with pade 1.3.3 Suppress plugin pion and ofmeet (no more needed, included in Padé) Update push and notification plugins

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+
 #Plugins install from Free-Solutions packages, please adjust to what you need, do not add or supress packages, stick to this list
 
 #installing openfire
+
 wget -O openfire_4.6.2_all.deb https://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_4.6.2_all.deb 
 dpkg -i openfire_4.6.2_all.deb 
 rm openfire_4.6.2_all.deb 
@@ -18,7 +20,7 @@ wget -O monitoring.jar https://webrtc.free-solutions.org/images/PLUGINS/monitori
 wget -O registration.jar https://webrtc.free-solutions.org/images/PLUGINS/registration.jar
 wget -O externalservicediscovery.jar https://webrtc.free-solutions.org/images/PLUGINS/externalservicediscovery.jar
 wget -O pushnotification.jar https://webrtc.free-solutions.org/images/PLUGINS/pushnotification.jar
-wget -O pionturn.jar https://webrtc.free-solutions.org/images/PLUGINS/pionturn.jar
+#wget -O pionturn.jar https://webrtc.free-solutions.org/images/PLUGINS/pionturn.jar
 wget -O pade.jar https://webrtc.free-solutions.org/images/PLUGINS/pade.jar
 
  
