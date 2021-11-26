84e501bb37e846b54b6a58a19cfc8082f78e9c20
root <root@psad@free-solutions.org>
2021-11-26 21:31:11 +0100

    Update to Openfire 4.6.4 and Padé 1.5.0

--- pre-conf.sh
+++ pre-conf.sh
@@ -6,21 +6,21 @@ set -e
 
 #installing openfire
 
-wget -O openfire_4.6.3_all.deb https://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_4.6.3_all.deb 
-dpkg -i openfire_4.6.3_all.deb 
-rm openfire_4.6.3_all.deb 
+wget -O openfire_4.6.4_all.deb https://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_4.6.4_all.deb 
+dpkg -i openfire_4.6.4_all.deb 
+rm openfire_4.6.4_all.deb 
 
 
 #installing Openfire plugins 
 cd /usr/share/openfire/plugins/
-wget -O bookmarks.jar https://webrtc.free-solutions.org/images/PLUGINS/bookmarks.jar
-wget -O fastpath.jar https://webrtc.free-solutions.org/images/PLUGINS/fastpath.jar
-wget -O httpfileupload.jar https://webrtc.free-solutions.org/images/PLUGINS/httpfileupload.jar
-wget -O monitoring.jar https://webrtc.free-solutions.org/images/PLUGINS/monitoring.jar
-wget -O registration.jar https://webrtc.free-solutions.org/images/PLUGINS/registration.jar
-wget -O externalservicediscovery.jar https://webrtc.free-solutions.org/images/PLUGINS/externalservicediscovery.jar
-wget -O pushnotification.jar https://webrtc.free-solutions.org/images/PLUGINS/pushnotification.jar
-wget -O pade.jar https://webrtc.free-solutions.org/images/PLUGINS/pade.jar
+wget -O bookmarks.jar https://www.igniterealtime.org/projects/openfire/plugins/1.0.3/bookmarks.jar 
+wget -O fastpath.jar https://www.igniterealtime.org/projects/openfire/plugins/4.4.5/fastpath.jar 
+wget -O httpfileupload.jar https://www.igniterealtime.org/projects/openfire/plugins/1.1.4/httpfileupload.jar 
+wget -O monitoring.jar https://www.igniterealtime.org/projects/openfire/plugins/2.2.1/monitoring.jar 
+wget -O registration.jar https://www.igniterealtime.org/projects/openfire/plugins/1.7.3/registration.jar 
+wget -O externalservicediscovery.jar https://www.igniterealtime.org/projects/openfire/plugins/1.0.1/externalservicediscovery.jar 
+wget -O pushnotification.jar https://www.igniterealtime.org/projects/openfire/plugins/0.9.0/pushnotification.jar 
+wget -O pade.jar https://www.igniterealtime.org/projects/openfire/plugins/1.5.0/pade.jar
 
  
  #Cleaning...
